07e02e6b3228e754c25b9365dfde4e450350678c
Darwin Ortiz Garcia <daor8303@colorado.edu>
2024-04-09 13:35:59 -0600

    Negative Testcase

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -2,6 +2,7 @@
 // <!-- Section 1 : Import Dependencies -->
 // *****************************************************
 
+
 const express = require('express'); // To build an application server or API
 const app = express();
 const handlebars = require('express-handlebars');
@@ -13,41 +14,48 @@ const session = require('express-session'); // To set the session object. To sto
 const bcrypt = require('bcrypt'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
 
+
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
 
+
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
-	extname: 'hbs',
-	layoutsDir: __dirname + '/views/layouts',
-	partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
 });
+
 
 // database configuration
 const dbConfig = {
-	host: 'db', // the database server
-	port: 5432, // the database port
-	database: process.env.POSTGRES_DB, // the database name
-	user: process.env.POSTGRES_USER, // the user account to connect with
-	password: process.env.POSTGRES_PASSWORD, // the password of the user account
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
 };
+
 
 const db = pgp(dbConfig);
 
+
 // test your database
 db.connect()
-	.then(obj => {
-		console.log('Database connection successful'); // you can view this message in the docker compose logs
-		obj.done(); // success, release the connection;
-	})
-	.catch(error => {
-		console.log('ERROR:', error.message || error);
-	});
+    .then(obj => {
+        console.log('Database connection successful'); // you can view this message in the docker compose logs
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
+
 
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
+
 
 // Register `hbs` as our view engine using its bound `engine()` function.
 app.engine('hbs', hbs.engine);
@@ -55,51 +63,62 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
+
 // initialize session variables
 app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		saveUninitialized: false,
-		resave: false,
-	})
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+    })
 );
+
 
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
+
 app.use(express.static(__dirname + '/public'));
+
 
 // *****************************************************
 // <!-- Section 4 : API Routes -->
 // *****************************************************
 
+
 // TODO - Include your API routes here
 app.get('/', (req, res) => {
-	res.redirect('/login');
+    res.redirect('/login');
 });
+
 
 app.get('/login', (req, res) => {
-	res.render('pages/login');
+    res.render('pages/login');
 });
+
 
 app.get('/settings', (req, res) => {
-	res.render('pages/settings');
+    res.render('pages/settings');
 });
+
 
 app.get('/register', (req, res) => {
-	res.render('pages/register');
+    res.render('pages/register');
 });
+
 
 app.get('/index', (req, res) => {
-	res.render('pages/index');
+    res.render('pages/index');
 });
 
+
 app.get('/savedpieces', (req, res) => {
-	res.render('pages/savedpieces');
+    res.render('pages/savedpieces');
 });
+
 
 // Register
 app.post('/register', async (req, res) => {
@@ -113,55 +132,63 @@ app.post('/register', async (req, res) => {
       res.redirect('/login');
     } catch (e) {
       console.log(e);
-      res.status(500).json({ status: 'error' });
+      res.status(400).json({ status: 'error' });
     }
   });
 
+
 // Login
 app.post('/login', async (req, res) => {
-	try {
-		const user = await db.one('SELECT username, password FROM users WHERE username = $1', [req.body.username]);
-		// check if password from request matches with password in DB
-		const match = await bcrypt.compare(req.body.password, user.password);
+    try {
+        const user = await db.one('SELECT username, password FROM users WHERE username = $1', [req.body.username]);
+        // check if password from request matches with password in DB
+        const match = await bcrypt.compare(req.body.password, user.password);
 
-		if(match) {
-			//save user details in session like in lab 7
-			req.session.user = user;
-			req.session.save();
-			res.redirect('/home');
-		}
-		else {
-			res.render('pages/login', {message: "Incorrect username or password"});
-		}
-	}
-	catch (e) {
-		console.log(e);
-		res.render('pages/login', {message: "Incorrect username or password"});
-	}
+
+        if(match) {
+            //save user details in session like in lab 7
+            req.session.user = user;
+            req.session.save();
+            res.redirect('/home');
+        }
+        else {
+            res.render('pages/login', {message: "Incorrect username or password"});
+        }
+    }
+    catch (e) {
+        console.log(e);
+        res.render('pages/login', {message: "Incorrect username or password"});
+    }
 });
+
 
 // Authentication Middleware.
 const auth = (req, res, next) => {
-	if (!req.session.user) {
-		// Default to login page.
-		return res.redirect('/login');
-	}
-	next();
+    if (!req.session.user) {
+        // Default to login page.
+        return res.redirect('/login');
+    }
+    next();
 };
 app.get('/welcome', (req, res) => {
-	res.json({status: 'success', message: 'Welcome!'});
+    res.json({status: 'success', message: 'Welcome!'});
   });
 // Authentication Required
 app.use(auth);
 
+
 app.get('/home', async (req, res) => {
-	res.render('pages/home')
+    res.render('pages/home')
 });
 
+
 app.get('/logout', async (req, res) => {
-	req.session.destroy();
-	res.render('pages/logout', {message: "Logged out Successfully"});
+    req.session.destroy();
+    res.render('pages/logout', {message: "Logged out Successfully"});
 });
+
+
+
 
 
 
--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -49,4 +49,24 @@ describe('Testing Add User API', () => {
       });
   });
 });
+// Example Negative Testcase :
+// API: /add_user
+// Input: {id: 5, name: 'John Doe', dob: '2020-02-20'}
+// Expect: res.status == 200 and res.body.message == 'Success'
+// Result: This test case should pass and return a status 200 along with a "Success" message.
+// Explanation: The testcase will call the /add_user API with the following input
+// and expects the API to return a status of 200 along with the "Success" message.
+describe('Testing Add User API', () => {
+  it('negative : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: 'JohnnyDoe', password: 'password'})
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.status).to.equals('error');
+        done();
+      });
+  });
+});
 // ********************************************************************************
