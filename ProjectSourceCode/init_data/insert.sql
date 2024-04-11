eb04edc5c696e1da6f7cb6cc6c67e704d2c630d3
Kevin <kevinshu.colorado@gmail.com>
2024-04-11 13:43:05 -0600

    Added create.sql and insert.sql
    Please note the minor changes to the database structure, namely around colors and patterns.

---
+++ ProjectSourceCode/init_data/insert.sql
@@ -0,0 +1,63 @@
+INSERT INTO categories VALUES
+	('top'),
+	('bottom'),
+	('full_body'),
+	('footwear'),
+	('accessory'),
+	('outerwear');
+
+INSERT INTO subcategories VALUES
+	('short_sleeve'),
+	('long_sleeve'),
+	('sweater'),
+	('short_pants'),
+	('long_pants'),
+	('skirt'),
+	('one_piece'),
+	('dress'),
+	('sneakers'),
+	('boots'),
+	('heels'),
+	('jewelry'),
+	('headwear'),
+	('eyewear'),
+	('socks'),
+	('coat'),
+	('vest'),
+	('jacket');
+
+INSERT INTO styles VALUES
+	('formal'),
+	('casual'),
+	('office'),
+	('athletic'),
+	('party'),
+	('fancy'),
+	('hot'),
+	('rain'),
+	('snow');
+
+INSERT INTO warmths VALUES
+	('none'),
+	('light'),
+	('medium'),
+	('heavy');
+
+INSERT INTO colors VALUES
+	('red'),
+	('orange'),
+	('yellow'),
+	('green'),
+	('blue'),
+	('indigo'),
+	('violet'),
+	('black'),
+	('white');
+
+INSERT INTO patterns VALUES
+	('solid'),
+	('plaid'),
+	('striped'),
+	('polka dot'),
+	('graphic'),
+	('camo');
