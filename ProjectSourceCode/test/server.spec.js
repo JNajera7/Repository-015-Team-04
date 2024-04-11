8d37c3b3d187f15cf903c9481f54f834464ce5f3
Jesus Najera <najera.jesus14@gmail.com>
2024-04-11 10:02:32 -0600

    added final 2 test cases for lab 11

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -69,3 +69,38 @@ describe('Testing Add User API', () => {
   });
 });
 // ********************************************************************************
+// ***************** TODO: 2 MORE UNIT TEST CASES USING OTHER ENDPOINT ******************
+// Example Positive Testcase :
+// API: /login
+// Expect: redirects to '/login' and status == 200
+// Result: This test case should pass and redirect the user to the login page
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/login" route should render with an html response', done => {
+    chai
+      .request(server)
+      .get('/login') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  });
+});
+// Example Negative Testcase :
+// API: /log
+// Expect: attempts to redirect to non-existent '/log' endpoint and status == 400
+// Result: This test case should pass and redirect the user to the login page
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/log" route', done => {
+    chai
+      .request(server)
+      .get('/log')
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.status).to.equals('error');
+        done();
+      });
+  });
+});
